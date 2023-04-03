8bc4f80b039d1a4fb8229abc9c3c73d85619e027
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-04-03 15:48:55 +0500

    default browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,9 @@ from selene import Browser, Config
 from utils import attach
 
 
+DEFAULT_BROWSER_VERSION = "100.0"
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_version",
@@ -22,6 +25,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def setup_browser(request):
     browser_version = request.config.getoption("--browser_version")
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
