00abd06705f5a8b191106457c1ab7ea1bcb9a37a
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-04-03 16:10:05 +0500

    add dotenv

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+from dotenv import load_dotenv
 
 from utils import attach
 
@@ -22,10 +25,17 @@ def pytest_addoption(parser):
     # )
 
 
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope="function")
 def setup_browser(request):
     browser_version = request.config.getoption("--browser_version")
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_version = (
+        browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    )
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -33,8 +43,12 @@ def setup_browser(request):
         "selenoid:options": {"enableVNC": True, "enableVideo": True},
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options,
     )
     browser = Browser(Config(driver))
