98db926756eba31e741e14b1b074e6336e95179f
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-04-03 14:28:09 +0500

    add add_video function with selenoid records

--- utils/attach.py
+++ utils/attach.py
@@ -34,7 +34,7 @@ def add_html(browser):
 
 def add_video(browser):
     video_url = (
-        "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + "mp4"
+        "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
     )
     html = (
         "<html><body><video width='100%' height='100%' controls autoplay><source src='"
@@ -42,8 +42,5 @@ def add_video(browser):
         + "' type='video/mp4'></video></body></html>"
     )
     allure.attach(
-        body=html,
-        name="video_" + browser.driver.session_id,
-        attachment_type=AttachmentType.HTML,
-        extension=".html",
+        html, "video_" + browser.driver.session_id, AttachmentType.HTML, ".html"
     )
